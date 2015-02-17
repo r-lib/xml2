065298440bba1fc1a76e22808dfac1b09a007ecd
hadley <h.wickham@gmail.com>
2015-02-17 15:44:40 -0600

    Make xml_root generic & improve docs

--- R/xml_root.R
+++ R/xml_root.R
@@ -1,8 +1,28 @@
-#' Extract the root node from a document
+#' Extract the root node.
 #'
-#' @param x An xml_doc
+#' @param x A document, a node or a nodeset.
+#' @return If \code{x} is a document, returns the root node. If \code{x}
+#'   is a node or nodeset, returns the input unchanged.
 #' @export
-xml_root <- function(x) {
+#' @examples
+#' x <- xml_parse("<foo> <bar> 1 </bar> </foo>")
+#' xml_root(x)
+#' xml_root(x)
+xml_root <- function(x, ...) {
+  UseMethod("xml_root")
+}
+
+#' @export
+xml_root.xml_doc <- function(x, ...) {
   xml_node(doc_root(x$doc), x$doc)
 }
 
+#' @export
+xml_root.xml_node <- function(x, ...) {
+  x
+}
+
+#' @export
+xml_root.xml_nodeset <- function(x, ...) {
+  x
+}
