be42cef792769024d827a3dddc6e52f043ad90a8
hadley <h.wickham@gmail.com>
2015-02-17 15:51:10 -0600

    Enhance xml_children

--- R/xml_children.R
+++ R/xml_children.R
@@ -1,19 +1,25 @@
-
-#' Collect children nodes
+#' Extract children.
+#'
+#' @param x A document, node or nodeset.
+#' @return A nodeset
 #' @export
-xml_children <- function(x) {
+#' @examples
+#' x <- xml_parse("<foo> <bar><boo /></bar> 123 <baz/> </foo>")
+#' (kids <- xml_children(x))
+#' (grand_kids <- xml_children(kids))
+xml_children <- function(x, ...) {
   UseMethod("xml_children")
 }
 
 #' @export
-xml_children.xml_node <- function(x) {
+xml_children.xml_node <- function(x, ...) {
   xml_nodeset(node_children(x$node), x$doc)
 }
 #' @export
-xml_children.xml_nodeset <- function(x) {
-  lapply(x, xml_children)
+xml_children.xml_nodeset <- function(x, ...) {
+  unlist(lapply(x, xml_children), recursive = FALSE)
 }
 #' @export
-xml_children.xml_doc <- function(x) {
+xml_children.xml_doc <- function(x, ...) {
   xml_children(xml_root(x))
 }
