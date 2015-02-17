a4eeea4fd683b6bcfa02a7e532cc152f0d9f9481
hadley <h.wickham@gmail.com>
2015-02-17 15:48:13 -0600

    Enhance xml_name

--- R/xml_name.R
+++ R/xml_name.R
@@ -1,14 +1,30 @@
 #' The (tag) name of an xml element.
 #'
 #' @param x A document, node, or node set.
+#' @return A character vector.
 #' @export
 #' @examples
 #' x <- xml_parse("<bar>123</bar>")
 #' xml_name(x)
-xml_name <- function(x) {
+#'
+#' y <- xml_parse("<bar><baz>1</baz>abc<foo /></bar>")
+#' z <- xml_children(y)
+#' xml_name(xml_children(y))
+xml_name <- function(x, ...) {
   UseMethod("xml_name")
 }
+
 #' @export
-xml_name.xml_node <- function(x) node_name(x$node)
+xml_name.xml_node <- function(x, ...) {
+  node_name(x$node)
+}
+
 #' @export
-xml_name.xml_doc <- function(x) node_name(xml_root(x)$node)
+xml_name.xml_doc <- function(x, ...) {
+  node_name(xml_root(x)$node)
+}
+
+#' @export
+xml_name.xml_nodeset <- function(x, ...) {
+  vapply(x, xml_name, character(1))
+}
