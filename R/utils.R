830ca6e973a20e1f1783240ebadc64d61aed46fe
hadley <h.wickham@gmail.com>
2015-02-18 12:01:44 -0600

    Revert importing of %>%. Want to minimise deps

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,1 @@
 `%||%` <- function(a, b) if (is.null(a)) b else a
-
-#' Pipe operator
-#'
-#' See \code{\link[magrittr]{\%>\%}} for more details.
-#'
-#' @name %>%
-#' @rdname pipe
-#' @keywords internal
-#' @export
-#' @importFrom magrittr %>%
-#' @usage lhs \%>\% rhs
-NULL
