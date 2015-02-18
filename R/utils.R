c643daca265e9aed8a0b69b31717fcb56bf3d1ca
hadley <h.wickham@gmail.com>
2015-02-18 11:24:14 -0600

    Import and re-export pipe

--- R/utils.R
+++ R/utils.R
@@ -1,1 +1,13 @@
 `%||%` <- function(a, b) if (is.null(a)) b else a
+
+#' Pipe operator
+#'
+#' See \code{\link[magrittr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
