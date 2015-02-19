407bfc76c376094472384c040dae20622a36387b
hadley <h.wickham@gmail.com>
2015-02-19 11:14:27 -0600

    Send XML errors through R

---
+++ src/xml2_init.cpp
@@ -0,0 +1,27 @@
+#include <Rcpp.h>
+#include <R_ext/Rdynload.h>
+#include <stdio.h>
+#include <libxml/xmlversion.h>
+#include <libxml/xmlerror.h>
+
+void handleError(void* userData, xmlError* error) {
+  std::string message = std::string(error->message);
+  message.resize(message.size() - 1); // trim off trailing newline
+
+  if (error->level <= 2) {
+    Rcpp::warning("%s [%i]", message, error->code);
+  } else {
+    Rcpp::stop("%s [%i]", message, error->code);
+  }
+}
+
+extern "C" {
+
+  void R_init_xml2(DllInfo *info) {
+    // Check that header and libs are compatible
+    LIBXML_TEST_VERSION
+
+    xmlSetStructuredErrorFunc(NULL, handleError);
+  }
+
+};
