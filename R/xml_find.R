b7fa08a1ce5e9d9d47d3f1de57e63d1bce66b5ba
hadley <h.wickham@gmail.com>
2015-02-19 12:22:41 -0600

    Xpath and namespaces

--- R/xml_find.R
+++ R/xml_find.R
@@ -1,6 +1,9 @@
-#' Search for matching xpath expressions.
+#' Find nodes that match an xpath expression.
 #'
-#' @param xpath A string containing a xpath expression.
+#' Xpath is like regular expressions for trees - it's worth learning if
+#' you're trying to extract nodes from arbitrary locations in a document.
+#'
+#' @param xpath A string containing a xpath (1.0) expression.
 #' @inheritParams xml_name
 #' @export
 #' @examples
@@ -14,6 +17,16 @@
 #' (bar <- xml_find(x, ".//bar"))
 #' xml_find(bar, ".//baz")
 #' xml_find(bar, "//baz")
+#'
+#' # If the document uses namespaces, you'll need use xml_ns:
+#' x <- xml('
+#'  <root xmlns:f = "http://foo.com" xmlns:g = "http://bar.com">
+#'    <f:doc><g:baz /></f:doc>
+#'    <f:doc><g:baz /></f:doc>
+#'  </root>
+#' ')
+#' xml_find(x, ".//f:doc")
+#' xml_find(x, ".//f:doc", xml_ns(x))
 xml_find <- function(x, xpath, ns = character(), ...) {
   UseMethod("xml_find")
 }
