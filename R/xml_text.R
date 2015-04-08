29ed83f85706f1ab1342e565fab241da7cf76e5f
hadley <h.wickham@gmail.com>
2015-04-08 08:38:47 -0500

    Fixes for xml_text

--- R/xml_text.R
+++ R/xml_text.R
@@ -6,12 +6,21 @@
 #' x <- xml("<p>This is some text. This is <b>bold!</b></p>")
 #' xml_text(x)
 #' xml_text(xml_children(x))
+#'
+#' x <- xml("<x>This is some text. <x>This is some nested text.</x></x>")
+#' xml_text(x)
+#' xml_text(xml_find(x, "//x"))
 #' @export
 xml_text <- function(x) {
   UseMethod("xml_text")
 }
 
 #' @export
+xml_text.xml_node <- function(x) {
+  node_text(x$node)
+}
+
+#' @export
 xml_text.xml_nodeset <- function(x) {
-  vapply(x$node, node_text, FUN.VALUE = character(1))
+  vapply(x, xml_text, FUN.VALUE = character(1))
 }
