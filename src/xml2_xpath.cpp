6e78560de98fc569124fc8141b12ca6589af4614
hadley <h.wickham@gmail.com>
2015-04-09 07:56:12 -0500

    Refactor node_find into a class

--- src/xml2_xpath.cpp
+++ src/xml2_xpath.cpp
@@ -5,46 +5,77 @@
 #include "xml2_types.h"
 using namespace Rcpp;
 
-// [[Rcpp::export]]
-Rcpp::List node_find(XPtrNode node, XPtrDoc doc, std::string xpath, CharacterVector nsMap) {
-  boost::shared_ptr<xmlXPathContext> context(
-      xmlXPathNewContext(doc.get()),
-      xmlXPathFreeContext
-  );
+class XmlSeeker {
+  xmlXPathContext* context_;
+  xmlXPathObject* result_;
 
-  // Set context to current node
-  context->node = node.get();
+public:
 
-  // Register namespacese
-  if (nsMap.size() > 0) {
+  XmlSeeker(xmlDoc* doc, xmlNode* node) : result_(NULL) {
+    context_ = xmlXPathNewContext(doc);
+    // Set context to current node
+    context_->node = node;
+  }
+
+  void registerNamespace(CharacterVector nsMap) {
+    if (nsMap.size() == 0)
+      return;
+
     CharacterVector prefix = as<CharacterVector>(nsMap.attr("names"));
 
     for (int i = 0; i < nsMap.size(); ++i) {
       xmlChar* prefixI = (xmlChar*) CHAR(STRING_ELT(prefix, i));
       xmlChar* urlI = (xmlChar*) CHAR(STRING_ELT(nsMap, i));
 
-      if (xmlXPathRegisterNs(context.get(), prefixI, urlI) != 0)
+      if (xmlXPathRegisterNs(context_, prefixI, urlI) != 0)
         stop("Failed to register namespace (%s <-> %s)", prefixI, urlI);
     }
   }
 
-  boost::shared_ptr<xmlXPathObject> result(
-    xmlXPathEval((xmlChar*) xpath.c_str(), context.get()),
-    xmlXPathFreeObject
-  );
-  // Return an empty list if there are no matches
-  if (result.get() == NULL)
-    return Rcpp::List::create();
+  void search(std::string xpath) {
+    result_ = xmlXPathEval((xmlChar*) xpath.c_str(), context_);
 
-  if (result->type != XPATH_NODESET)
-    Rcpp::stop("Currently only nodeset results are supported");
-
-  xmlNodeSetPtr nodes = result->nodesetval;
-  int n = nodes->nodeNr;
-  List out(n);
-  for (int i = 0; i < nodes->nodeNr; i++) {
-    out[i] = XPtrNode(nodes->nodeTab[i]);
+    if (result_ != NULL && result_->type != XPATH_NODESET)
+      Rcpp::stop("Currently only nodeset results are supported");
   }
 
-  return out;
+  int n_matches() {
+    if (result_ == NULL)
+      return 0;
+
+    xmlNodeSetPtr nodes = result_->nodesetval;
+    return (nodes == NULL) ? 0 : nodes->nodeNr;
+  }
+
+  List matches() {
+    int n = n_matches();
+    List out(n);
+
+    xmlNodeSetPtr nodes = result_->nodesetval;
+    for (int i = 0; i < nodes->nodeNr; i++) {
+      out[i] = XPtrNode(nodes->nodeTab[i]);
+    }
+    return out;
+  }
+
+  ~XmlSeeker() {
+    try {
+      xmlXPathFreeContext(context_);
+      if (result_ != NULL)
+        xmlXPathFreeObject(result_);
+    } catch (...) {}
+  }
+
+};
+
+// [[Rcpp::export]]
+Rcpp::List node_find(XPtrNode node, XPtrDoc doc, std::string xpath, CharacterVector nsMap) {
+  XmlSeeker seeker(doc.get(), node.get());
+  seeker.registerNamespace(nsMap);
+  seeker.search(xpath);
+
+  if (seeker.n_matches() == 0)
+    return List();
+
+  return seeker.matches();
 }
