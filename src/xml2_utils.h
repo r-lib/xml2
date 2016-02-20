76142c6de886b563d731378c1372b6c9014dc348
Jim Hester <james.f.hester@gmail.com>
2016-02-20 08:08:49 -0500

    Use indexes into vectors rather than pointers.
    As the pointers would be invalidated when the vector is re-allocated.

--- src/xml2_utils.h
+++ src/xml2_utils.h
@@ -5,7 +5,7 @@
 #include <libxml/tree.h>
 #include <boost/shared_ptr.hpp>
 #include <boost/typeof/typeof.hpp>
-#include <map>
+#include <boost/unordered_map.hpp>
 
 inline xmlChar* asXmlChar(std::string x) {
   return (xmlChar*) x.c_str();
@@ -59,12 +59,15 @@ public:
 
 class NsMap {
 
-  // We only store a pointer to the strings in url2prefix to avoid an extra duplication.
-  typedef std::multimap<std::string, std::string> prefix2url_t;
-  typedef std::map<std::string, const std::string*> url2prefix_t;
+  // We only store the index to avoid duplicating the data
+  typedef boost::unordered_multimap<std::string, std::size_t> prefix2url_t;
+  typedef boost::unordered_multimap<std::string, std::size_t> url2prefix_t;
 
   prefix2url_t prefix2url;
   url2prefix_t url2prefix;
+
+  std::vector<std::string> prefix_;
+  std::vector<std::string> url_;
 
   public:
   NsMap() {
@@ -73,7 +76,7 @@ class NsMap {
   // Initialise from an existing character vector
   NsMap(Rcpp::CharacterVector x) {
     BOOST_AUTO(names, Rcpp::as<Rcpp::CharacterVector>(x.attr("names")));
-    for (int i = 0; i < x.size(); ++i) {
+    for (std::size_t i = 0; i < x.size(); ++i) {
       add(std::string(names[i]), std::string(x[i]));
     }
   }
@@ -85,7 +88,7 @@ class NsMap {
   std::string findPrefix(std::string url) {
     BOOST_AUTO(it, url2prefix.find(url));
     if (it != url2prefix.end()) {
-      return *it->second;
+      return prefix_.at(it->second);
     }
 
     Rcpp::stop("Couldn't find prefix for url %s", url);
@@ -95,7 +98,7 @@ class NsMap {
   std::string findUrl(std::string prefix) {
     BOOST_AUTO(it, prefix2url.find(prefix));
     if (it != prefix2url.end()) {
-      return it->second;
+      return url_.at(it->second);
     }
 
     Rcpp::stop("Couldn't find url for prefix %s", prefix);
@@ -110,17 +113,23 @@ class NsMap {
     if (hasUrl(url))
       return false;
 
-    // The return value of multimap.insert is an iterator of the inserted
-    // element. We store the address of the key string as the value in
-    // url2prefix to avoid duplicating the data.
-    BOOST_AUTO(p, prefix2url.insert(prefix2url_t::value_type(prefix, url)));
-    url2prefix.insert(url2prefix_t::value_type(url, &(p->first)));
+    // Add the valuse to the vectors and add the index to the maps.
+    // We never delete values so we don't have to worry about keeping the
+    // indexes updated.
+    std::size_t i = prefix_.size();
+    prefix_.push_back(prefix);
+    url_.push_back(url);
+    prefix2url.insert(prefix2url_t::value_type(prefix, i));
+    url2prefix.insert(url2prefix_t::value_type(url, i));
 
     return true;
   }
 
   Rcpp::CharacterVector out() {
-    return Rcpp::wrap(prefix2url);
+    Rcpp::CharacterVector out = Rcpp::wrap(url_);
+    out.attr("names") = Rcpp::wrap(prefix_);
+
+    return out;
   }
 };
 
