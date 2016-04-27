a8690f985ef01c46ff2d6a0bae65ca91391711c8
Jim Hester <james.f.hester@gmail.com>
2016-04-27 12:30:45 -0400

    Tests for xml_contents

--- tests/testthat/test-modify-xml.R
+++ tests/testthat/test-modify-xml.R
@@ -1,5 +1,8 @@
 context("modify nodes")
 
+test_that("modifying existing nodes works", {
+
+})
 test_that("modifying nodes works", {
   x <- read_xml("<x><y/></x>")
   node <- xml_find_one(x, "//x")
@@ -30,4 +33,17 @@ test_that("Creating nodes works", {
   expect_equal(xml_name(child), "a")
   expect_equal(xml_text(child), "Zombo.com!")
   expect_equal(xml_attrs(child), c(href = "http://www.zombo.com"))
+})
+
+test_that("xml_contents<- modifies xml content", {
+  x <- read_xml("test3.xml")
+  y <- xml_find_all(x, "//discarded")
+  xml_contents(y) <- "discarded"
+  tmp_file <- tempfile()
+
+  write_xml(x, tmp_file)
+
+  # verify output matches that from the xpath2 utility in libxml2
+  # https://raw.githubusercontent.com/GNOME/libxml2/3eaedba1b64180668fdab7ad2eba549586017bf3/doc/examples/xpath2.res
+  expect_equal(readLines(tmp_file), readLines("xpath2.res"))
 })
