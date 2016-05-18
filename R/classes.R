51453919e0e1f5676a1bd7a77e56800a3fd637b5
Jim Hester <james.f.hester@gmail.com>
2016-05-18 16:36:57 -0400

    Change the default to lookup the documents namespace

--- R/classes.R
+++ R/classes.R
@@ -41,7 +41,6 @@ print.xml_missing <- function(x, width = getOption("width"), max_n = 20, ...) {
 xml_document <- function(doc) {
   x <- xml_node(doc_root(doc), doc)
   class(x) <- c("xml_document", class(x))
-  x$namespace <- xml_ns(x)
   x
 }
 
