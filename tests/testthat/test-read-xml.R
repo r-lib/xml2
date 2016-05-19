2137fefd91444ddac4a7560d28f7af034f8b0e48
Jim Hester <james.f.hester@gmail.com>
2016-05-19 09:40:07 -0400

    Add tests for passing options

--- tests/testthat/test-read-xml.R
+++ tests/testthat/test-read-xml.R
@@ -12,3 +12,15 @@ test_that("parse_options errors when given an invalid option", {
   expect_error(read_html("lego.html.bz2", options = "INVALID"),
     "`options` INVALID is not a valid option")
 })
+
+test_that("read_html properly passes parser arguments", {
+  lego <- read_html("lego.html.bz2", options = c("RECOVER", "NOERROR"))
+
+  expect_equal(as_list(xml_children(lego)[[1]])[[1]],
+    "\n\n        \n    \n            ")
+
+  lego <- read_html("lego.html.bz2", options = c("RECOVER", "NOERROR", "NOBLANKS"))
+
+  expect_equal(as_list(xml_children(lego)[[1]])[[1]][[1]],
+    "var ue_t0=window.ue_t0||+new Date();")
+})
