54c1a86b643425827681205a1078465618ee98a6
Jeroen Ooms <jeroenooms@gmail.com>
2016-06-09 19:46:54 +0200

    upgrade libxml2 for windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against libxml2 from Rtools
-if (!file.exists("../windows/libxml2-2.9.3/include/libxml2/libxml/parser.h")) {
+if (!file.exists("../windows/libxml2-2.9.4/include/libxml2/libxml/parser.h")) {
   if(getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/libxml2/archive/v2.9.3.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/libxml2/archive/v2.9.4.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
--- windows/libxml2-2.9.3/readme.md
+++
@@ -1,2 +0,0 @@
-Patched libxml2 versions from msys2.
-Client must build with `-DLIBXML_STATIC` to link against static libraries.
