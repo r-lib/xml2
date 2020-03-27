9347b3eb2fa05d78b6f8892669e1b4ce96c2bcaa
Jim Hester <james.f.hester@gmail.com>
2020-03-27 13:01:44 -0400

    Convert node_write_character

--- R/classes.R
+++ R/classes.R
@@ -15,7 +15,7 @@ xml_node <- function(node = NULL, doc = NULL) {
 #' @export
 as.character.xml_node <- function(x, ..., options = "format", encoding = "UTF-8") {
   options  <- parse_options(options, xml_save_options())
-  node_write_character(x$node, options = options, encoding = encoding)
+  .Call(node_write_character, x$node, encoding, options)
 }
 
 #' @export
