aa3e9675ca3cdf700aebff05833b7801fce20956
Jim Hester <james.f.hester@gmail.com>
2020-03-27 16:19:50 -0400

    Convert read_connection_
    I rewrote this to just read the data into a regular vector. Should be a
    bit faster and put less pressure on the R garbage collector.

--- src/connection.cpp
+++ src/connection.cpp
@@ -1,37 +1,36 @@
 #include <Rcpp.h>
-using namespace Rcpp;
 
 // Wrapper around R's read_bin function
-RawVector read_bin(RObject con, int bytes = 64 * 1024) {
-  Rcpp::Environment baseEnv = Rcpp::Environment::base_env();
-  Rcpp::Function readBin = baseEnv["readBin"];
-
-  RawVector out = Rcpp::as<RawVector>(readBin(con, "raw", bytes));
-  return out;
+SEXP read_bin(SEXP con, double bytes = 64 * 1024) {
+  SEXP e;
+  PROTECT(e = Rf_lang4(Rf_install("readBin"), con, Rf_mkString("raw"), Rf_ScalarInteger(bytes)));
+  SEXP res = Rf_eval(e, R_GlobalEnv);
+  UNPROTECT(1);
+  return res;
 }
 
 // Read data from a connection in chunks and then combine into a single
 // raw vector.
 //
-// [[Rcpp::export]]
-RawVector read_connection_(RObject con, int chunk_size = 64 * 1024) {
-  std::vector<RawVector> chunks;
+// [[export]]
+extern "C" SEXP read_connection_(SEXP con_sxp, SEXP read_size_sxp) {
+  std::vector<uint8_t> buffer;
+  double read_size = REAL(read_size_sxp)[0];
 
-  RawVector chunk;
-  while((chunk = read_bin(con, chunk_size)).size() > 0)
-    chunks.push_back(chunk);
-
-  size_t size = 0;
-  for (size_t i = 0; i < chunks.size(); ++i)
-    size += chunks[i].size();
-
-  RawVector out(size);
-  size_t pos = 0;
-  for (size_t i = 0; i < chunks.size(); ++i) {
-    memcpy(RAW(out) + pos, RAW(chunks[i]), chunks[i].size());
-    pos += chunks[i].size();
+  SEXP chunk = read_bin(con_sxp, read_size);
+  R_xlen_t chunk_size = Rf_xlength(chunk);
+  while(chunk_size > 0) {
+    std::copy(RAW(chunk), RAW(chunk) + chunk_size, std::back_inserter(buffer));
+    chunk = read_bin(con_sxp, read_size);
+    chunk_size = Rf_xlength(chunk);
   }
+
+  size_t size = buffer.size();
+
+  SEXP out = PROTECT(Rf_allocVector(RAWSXP, size));
+  std::copy(buffer.begin(), buffer.end(), RAW(out));
+
+  UNPROTECT(1);
 
   return out;
 }
-
