44eddbeadf6459d6f1fe808a1eb7af716e0cbec9
Maximilian Girlich <maximilian.girlich@metoda.com>
2023-09-04 11:10:26 +0000

    Remove unused `R_ReadConnection()`

--- src/connection.h
+++ src/connection.h
@@ -14,18 +14,6 @@ SEXP write_bin(SEXP data, SEXP con);
 
 inline SEXP R_GetConnection(SEXP con) { return con; }
 
-inline size_t R_ReadConnection(SEXP con, void* buf, size_t n) {
-  SEXP res = PROTECT(read_bin(con, n));
-
-  R_xlen_t size = Rf_xlength(res);
-
-  memcpy(buf, RAW(res), size);
-
-  UNPROTECT(1);
-
-  return Rf_xlength(res);
-}
-
 inline size_t R_WriteConnection(SEXP con, void* buf, size_t n) {
   SEXP payload = PROTECT(Rf_allocVector(RAWSXP, n));
 
