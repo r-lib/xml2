e4a54aeb0fc1898933e99260499a5c9e8b9b626b
KNnut <9387720+KNnut@users.noreply.github.com>
2023-11-30 22:21:41 +0800

    Fix compilation with libxml2 2.12.0 (#417)
    Fixes #418

--- src/xml2_schema.cpp
+++ src/xml2_schema.cpp
@@ -9,7 +9,18 @@
 #include "xml2_types.h"
 #include "xml2_utils.h"
 
+/* * *
+ * Author: Nick Wellnhofer <wellnhofer@aevum.de>
+ * Date:   Tue, 24 Oct 2023 15:02:36 +0200
+ * https://github.com/GNOME/libxml2/commit/61034116d0a3c8b295c6137956adc3ae55720711
+ *
+ * error: Make more xmlError structs constant
+ */
+#if defined(LIBXML_VERSION) && (LIBXML_VERSION >= 21200)
+void handleSchemaError(void* userData, const xmlError* error) {
+#else
 void handleSchemaError(void* userData, xmlError* error) {
+#endif
   std::vector<std::string> * vec = (std::vector<std::string> *) userData;
   std::string message = std::string(error->message);
   message.resize(message.size() - 1);
@@ -21,8 +32,6 @@ extern "C" SEXP doc_validate(SEXP doc_sxp, SEXP schema_sxp) {
 
   XPtrDoc doc(doc_sxp);
   XPtrDoc schema(schema_sxp);
-
-  xmlLineNumbersDefault(1);
 
   BEGIN_CPP
 
